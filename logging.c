3353775956be9b7fc9a4f4b6d5f985a0b6a321e6
Ханин Алексей <akh.bla.bu@gmail.com>
2017-11-11 18:16:17 +0200

    Добавлены функции для логирования

--- logging.c
+++ logging.c
@@ -42,3 +42,28 @@ void writeLogU32(u32 data) {
 	toStringDec(data,tempStr);
 	writeLogTempString(tempStr);
 }
+
+void writeSymb(char symb) {
+    printf('%c', symb);
+}
+
+void writeLogByteArray(u08 sizeBytes, byte_ptr array){
+	static string_t str = NULL;
+	if(str != NULL) freeMem((byte_ptr)str);
+	u08 totalSize = sizeBytes*2 + sizeBytes + 1;
+	str = (string_t)allocMem(totalSize); // Выделяем память под строку + под пробелы между символами + байт конца
+	if(str == NULL){
+		writeLogStr("mem err in logging");
+		return;
+	}
+	memSet(str,totalSize,0);
+	u08 poz = 0;
+	for(u08 i = 0; i<sizeBytes; i++) {
+		toStringDec(array[i],&str[poz]);
+		poz=strSize(str);
+		str[poz] = ' ';
+		poz++;
+	}
+	str[poz] = '\0';
+	writeLogStr(str);
+}
--- logging.h
+++ logging.h
@@ -14,5 +14,6 @@ void writeLogTempString(string_t tempStr);
 void writeLogStr(string_t c_str);
 void writeLogFloat(float data);
 void writeLogU32(u32 data);
+void writeLogByteArray(u08 sizeBytes, byte_ptr array);
 
 #endif /* LOGGING_H_ */
