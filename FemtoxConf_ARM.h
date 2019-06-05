71cc558ce1eae814b88a4aae3d1c85bb545ce9e0
BlaBu <akh@akh.ua>
2019-06-05 17:33:27 +0300

    111

--- FemtoxConf_ARM.h
+++ FemtoxConf_ARM.h
@@ -30,10 +30,10 @@
 //#define NEED_BASE64
 //#define NEED_RANDOM
 #define NEED_CRC16
-//#define ENABLE_LOGGING
+#define ENABLE_LOGGING
 #define NEED_MATRIX
 //#define STANDART_MEMCPY_MEMSET
-//#define LOAD_STATISTIC /*Сколько времени мы находимся в IDLE процессе в сравнении с полным рабочим временем в секундах*/
+#define LOAD_STATISTIC /*Сколько времени мы находимся в IDLE процессе в сравнении с полным рабочим временем в секундах*/
 
 #define TASK_LIST_LEN 10U /*Длина очереди задач*/
 #define TIME_LINE_LEN 30U /*Максимальне количество системных таймеров*/
@@ -57,11 +57,9 @@
 
 #ifdef ALLOC_MEM
 #define HEAP_SIZE 7500UL /*6500*/
-//#define CHECK_ERRORS_FREE_MEMMORY
 #endif
 #ifdef ALLOC_MEM_LARGE
 #define HEAP_SIZE 10000UL /*6500*/
-//#define CHECK_ERRORS_FREE_MEMMORY
 #endif
 
 #ifdef CALL_BACK_TASK
